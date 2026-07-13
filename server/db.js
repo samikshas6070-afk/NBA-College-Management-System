5fae90e929ac94af5cc37746c0f8716439b72928
AbhinandanSakale <161486566+AbhinandanSakale@users.noreply.github.com>
2026-07-13 15:54:48 +0530

    Updated database connection for Render

--- server/db.js
+++ server/db.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "NBASoftware",
-  password: "1234",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
